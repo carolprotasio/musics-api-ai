fb99cca0651f3b2dfefcb3511476fd56a657d8d4
Carol Protásio <protasio.carol@gmail.com>
2025-06-12 17:42:23 -0300

    updating workflow

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,9 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/api',
     experimentalRunAllSpecs: true,
-   
+    defaultCommandTimeout: 8000, // ⬅ aumenta para 8 segundos
+    requestTimeout: 10000,       // ⬅ tempo de timeout de requisições
+    responseTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
